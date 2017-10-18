ad7db0bb104ea8061b998a1a74881c42f8842637
sstassin <sstassin@gmail.com>
2017-10-18 08:33:07 +0200

    Update ArchLinux Temp Script.sh

--- ArchLinux Temp Script.sh
+++ ArchLinux Temp Script.sh
@@ -130,15 +130,16 @@ wget https://raw.githubusercontent.com/sstassin/Raspberry-Archlinux-Setup/master
 wget https://raw.githubusercontent.com/sstassin/Raspberry-Archlinux-Setup/master/etc/openvpn/vpn-up.sh
 chmod u+x vpn-*.sh
 cd /etc/openvpn/client
-rm monitor-openvpn-connection.sh
-wget https://raw.githubusercontent.com/sstassin/Raspberry-Archlinux-Setup/master/etc/openvpn/client/monitor-openvpn-connection.sh
+rm monitor-openvpn-connection.sh 
+wget https://raw.githubusercontent.com/sstassin/Raspberry-Archlinux-Setup/master/etc/openvpn/client/monitor-openvpn-connection.sh 
 chmod u+x monitor-openvpn-connection.sh
 wget https://s3-us-west-1.amazonaws.com/heartbleed/linux/linux-files.zip
 cp Linux\ OpenVPN\ Updated\ files/ca.crt ./ca.crt
 cp Linux\ OpenVPN\ Updated\ files/Wdc.key ./Wdc.key
 nano auth.txt
 ln -sf Linux\ OpenVPN\ Updated\ files/TCP/Netherlands1-tcp.ovpn clien.conf
-monitor-openvpn-connection.sh
+bash monitor-openvpn-connection.sh
+tail -f /var/log/openvpn/client
 
 
 
