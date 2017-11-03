6a109d754c2465fb33bf25d257e4f68acb90644d
sstassin <sstassin@gmail.com>
2017-11-03 21:09:24 +0100

    Update OpenVpn-Setup.sh

--- OpenVpn-Setup.sh
+++ OpenVpn-Setup.sh
@@ -2,6 +2,7 @@
 #-Installing and configuring my OpenVpn connection
 # https://support.purevpn.com/linux-openvpn-command/
 mkdir -p /var/log/openvpn/client
+mkdir -p /etc/openvpn/client
 cd /etc/openvpn/
 rm vpn-*.sh
 wget https://raw.githubusercontent.com/sstassin/Raspberry-Archlinux-Setup/master/etc/openvpn/vpn-down.sh
@@ -13,9 +14,11 @@ wget https://raw.githubusercontent.com/sstassin/Raspberry-Archlinux-Setup/master
 wget https://raw.githubusercontent.com/sstassin/Raspberry-Archlinux-Setup/master/etc/openvpn/client/start-purevpn.sh
 chmod u+x *.sh
 wget https://s3-us-west-1.amazonaws.com/heartbleed/linux/linux-files.zip
+unzip linux-files.zip 
 cp Linux\ OpenVPN\ Updated\ files/ca.crt ./ca.crt
 cp Linux\ OpenVPN\ Updated\ files/Wdc.key ./Wdc.key
 nano auth.txt
-ln -sf Linux\ OpenVPN\ Updated\ files/TCP/Netherlands1-tcp.ovpn clien.conf
-bash monitor-openvpn-connection.sh
+ln -sf Linux\ OpenVPN\ Updated\ files/TCP/Netherlands1-tcp.ovpn client.conf
+./monitor-openvpn-connection.sh
+./start-purevpn.sh
 tail -f /var/log/openvpn/client
