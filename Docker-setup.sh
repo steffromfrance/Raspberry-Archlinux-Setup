7f43988e965900880dca3a8738246fd4f62399d6
sstassin <sstassin@gmail.com>
2017-11-13 21:48:57 +0100

    Update Docker-setup.sh

--- Docker-setup.sh
+++ Docker-setup.sh
@@ -1,1 +1,11 @@
 #! /bin/sh
+
+#-Installing Docker Hypriot Image to RPI
+#-http://blog.hypriot.com
+
+#-Setting up several usefull container
+
+#-DuckDns
+#-https://store.docker.com/community/images/lsioarmhf/duckdns
+docker create --name=duckdns -e PGID=1000 -e PUID=1000  -e SUBDOMAINS=stef2017 \
+  -e TOKEN=<MY TOKEN> -e TZ=Europe/Paris   lsioarmhf/duckdns
