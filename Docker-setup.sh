7648870e463e83693f51a750835931c04e147639
sstassin <sstassin@gmail.com>
2018-08-23 22:05:24 +0200

    Update Docker-setup.sh

--- Docker-setup.sh
+++ Docker-setup.sh
@@ -4,6 +4,15 @@
 #-http://blog.hypriot.com
 
 #-Setting up several usefull container
+#-More info at https://docs.docker.com/engine/reference/run/
+docker container ls
+docker image ls
+
+#Testing with hello world
+docker run --name hello-world hypriot/armhf-hello-world
+
+#Rpi Portainer : https://store.docker.com/community/images/hypriot/rpi-portainer
+docker run --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer:arm
 
 #-DuckDns
 #-https://store.docker.com/community/images/lsioarmhf/duckdns
