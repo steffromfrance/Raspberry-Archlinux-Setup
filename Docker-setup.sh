217dc5827b48ce7d38e384bbface7e6758f4dbd4
sstassin <sstassin@gmail.com>
2018-09-20 21:34:36 +0200

    Update Docker-setup.sh

--- Docker-setup.sh
+++ Docker-setup.sh
@@ -30,11 +30,50 @@ docker container stop rpi2-samba && docker container rm rpi2-samba
 docker run --name rpi2-samba -d \
   --restart='always' --hostname 'rpi2-filer' \
   -p 137:137/udp -p 138:138/udp -p 139:139 -p 445:445 -p 445:445/udp \
-  -v /media/pi/HDD1000G:/share/HDD1000G -v /var/log:/share/log \
+  -v /media/HDD1000G:/share/HDD1000G -v /var/log:/share/log \
   dastrasmue/rpi-samba:v3 \
   -u "pi:pi" \
-  -s "Logs:/share/log:ro" \
-  -s "HDD1000G:/share/HDD1000G:rw:pi"
+  -s "Rpi2 Logs Files (readonly):/share/log:ro" \
+  -s "HDD1000G (private for pi):/share/HDD1000G:rw:pi"
+
+#TransmissionOpenVPN : https://github.com/haugene/docker-transmission-openvpn
+echo "PUREVPNUSERNAME=myvpnusername" >> /etc/environnement
+echo "PUREVPNPWD=myvpnpwd" >> /etc/environnement
+docker container stop transmission && docker container rm transmission
+
+#Cloning and building
+git clone https://github.com/haugene/docker-transmission-openvpn.git
+cd docker-transmission-openvpn
+docker build -f Dockerfile.armhf -t  docker-transmission-openvpn:armhf .
+
+docker run --name=transmission --cap-add=NET_ADMIN --device=/dev/net/tun -d \
+              -v /media/pi/HDD1000G1/:/data \
+              -v /etc/localtime:/etc/localtime:ro \
+              -e OPENVPN_PROVIDER=PUREVPN \
+              -e OPENVPN_CONFIG=Netherlands1-tcp,Netherlands2-tcp,Norway-tcp,Sweden-tcp \
+              -e OPENVPN_USERNAME=$PUREVPNUSERNAME \
+              -e OPENVPN_PASSWORD=$PUREVPNPWD \
+              -e OPENVPN_OPTS=--inactive 3600 --ping 10 --ping-exit 60 \
+              --log-driver json-file \
+              --log-opt max-size=10m \
+              -p 9091:9091 \
+              docker-transmission-openvpn:armhf
+
+docker run --cap-add=NET_ADMIN --device=/dev/net/tun -d \
+			  -v /your/storage/path/:/data \
+              -v /etc/localtime:/etc/localtime:ro \
+              -e OPENVPN_PROVIDER=PIA \
+              -e OPENVPN_CONFIG=CA\ Toronto \
+              -e OPENVPN_USERNAME=user \
+              -e OPENVPN_PASSWORD=pass \
+              -e WEBPROXY_ENABLED=false \
+              -e LOCAL_NETWORK=192.168.0.0/16 \
+              --log-driver json-file \
+              --log-opt max-size=10m \
+              -p 9091:9091 \
+              haugene/transmission-openvpn
+
+
 
 
 #-DuckDns
