2a00abf2f5b09303fd3fb5c6d6493c669a4a9b52
sstassin <sstassin@gmail.com>
2018-10-14 22:59:59 +0200

    Update Docker-setup.sh

--- Docker-setup.sh
+++ Docker-setup.sh
@@ -21,6 +21,8 @@ docker run --name portainer --restart=always -d -p 9000:9000 \
  -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data \
  portainer/portainer:arm
  
+
+
 #Rpi Monitor : https://store.docker.com/community/images/neoraptor/rpi-monitor
 docker container stop rpimonitor && docker container rm rpimonitor
 docker run -d --name=rpimonitor --restart=always -p 8888:8888 neoraptor/rpi-monitor
@@ -37,10 +39,12 @@ docker run --name rpi2-samba -d \
   -s "HDD1000G:/share/HDD1000G:rw:pi"
 
 #TransmissionOpenVPN : https://github.com/haugene/docker-transmission-openvpn
-echo "PUREVPNUSERNAME=myvpnusername" >> /etc/environnement
-echo "PUREVPNPWD=myvpnpwd" >> /etc/environnement
-docker container stop transmission && docker container rm transmission
+echo "PUREVPNUSERNAME=myvpnusername" >> /etc/environment
+echo "PUREVPNPWD=myvpnpwd" >> /etc/environment
+echo "WSVPNUSERNAME=myvpnusername" >> /etc/environment
+echo "WSVPNPWD=myvpnpwd" >> /etc/environment
 
+docker container stop transmission && docker container rm transmission
 docker run --name=transmission --cap-add=NET_ADMIN --device=/dev/net/tun -d \
   --dns 8.8.8.8 --dns 8.8.4.4 --restart='always' \
   -v /media/HDD1000G/:/data \
@@ -50,6 +54,26 @@ docker run --name=transmission --cap-add=NET_ADMIN --device=/dev/net/tun -d \
   -e OPENVPN_USERNAME=$PUREVPNUSERNAME \
   -e OPENVPN_PASSWORD=$PUREVPNPWD \
   -e LOCAL_NETWORK=192.168.0.0/24 \
+  -e ENABLE_UFW=false \
+  -e OPENVPN_OPTS="--inactive 3601 --ping 10 --ping-exit 60" \
+  -e DROP_DEFAULT_ROUTE=true \
+  -e TRANSMISSION_DOWNLOAD_DIR="/data/Torrents-Downloads" \
+  -e TRANSMISSION_INCOMPLETE_DIR_ENABLED=false \
+  --log-driver json-file \
+  --log-opt max-size=10m \
+  -p 9091:9091 \
+  haugene/transmission-openvpn:dev-armhf
+
+docker container stop transmission-ws && docker container rm transmission-ws
+docker run --name=transmission-ws --cap-add=NET_ADMIN --device=/dev/net/tun -d \
+  --dns 8.8.8.8 --dns 8.8.4.4 --restart='always' \
+  -v /media/HDD1000G/:/data \
+  -v /etc/localtime:/etc/localtime:ro \
+  -e OPENVPN_PROVIDER=WINDSCRIBE \
+  -e OPENVPN_CONFIG=Netherlands-tcp,Norway-tcp,Sweden-tcp,Germany-tcp \
+  -e OPENVPN_USERNAME=$WSVPNUSERNAME \
+  -e OPENVPN_PASSWORD=$WSVPNPWD \
+  -e LOCAL_NETWORK=192.168.0.0/24 \
   -e ENABLE_UFW=true \
   -e OPENVPN_OPTS="--inactive 3601 --ping 10 --ping-exit 60" \
   -e DROP_DEFAULT_ROUTE=true \
@@ -57,7 +81,7 @@ docker run --name=transmission --cap-add=NET_ADMIN --device=/dev/net/tun -d \
   -e TRANSMISSION_INCOMPLETE_DIR_ENABLED=false \
   --log-driver json-file \
   --log-opt max-size=10m \
-  -p 9092:9091 \
+  -p 9091:9091 \
   haugene/transmission-openvpn:dev-armhf
 
 
