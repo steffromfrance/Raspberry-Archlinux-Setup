ab26097627f3a50b14014bc029ecb02250a6fad3
sstassin <sstassin@gmail.com>
2018-10-22 21:05:53 +0200

    Update rpi-cam.sh

--- rpi-cam.sh
+++ rpi-cam.sh
@@ -2,3 +2,10 @@
 
 #simple command line test
 raspistill -o ~/Desktop/picamera-testimage.jpg
+
+#Using RPI Cam Web Interface
+https://elinux.org/RPi-Cam-Web-Interface
+
+git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
+cd RPi_Cam_Web_Interface
+./install.sh
