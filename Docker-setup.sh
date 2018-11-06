7e047e03decc7516f0c88c04ae1d998345090c5e
sstassin <sstassin@gmail.com>
2018-11-06 21:42:25 +0100

    Update Docker-setup.sh

--- Docker-setup.sh
+++ Docker-setup.sh
@@ -84,6 +84,26 @@ docker run --name=transmission-ws --cap-add=NET_ADMIN --device=/dev/net/tun -d \
   -p 9091:9091 \
   haugene/transmission-openvpn:dev-armhf
 
+#using on Ubuntu Stef 2 Laptop
+docker container stop transmission-ws && docker container rm transmission-ws
+docker run --name=transmission-ws --cap-add=NET_ADMIN --device=/dev/net/tun \
+  --dns 8.8.8.8 --dns 8.8.4.4 --restart='always' \
+  -v /home/sstassin/transmission-docker-data/:/data \
+  -v /etc/localtime:/etc/localtime:ro \
+  -e OPENVPN_PROVIDER=WINDSCRIBE \
+  -e OPENVPN_CONFIG=Netherlands-tcp,Norway-tcp,Sweden-tcp,Germany-tcp \
+  -e OPENVPN_USERNAME=$WSVPNUSERNAME \
+  -e OPENVPN_PASSWORD=$WSVPNPWD \
+  -e LOCAL_NETWORK=192.168.0.0/24 \
+  -e ENABLE_UFW=true \
+  -e OPENVPN_OPTS="--inactive 3601 --ping 10 --ping-exit 60" \
+  -e DROP_DEFAULT_ROUTE=true \
+  -e TRANSMISSION_DOWNLOAD_DIR="/data/Torrents-Downloads" \
+  -e TRANSMISSION_INCOMPLETE_DIR_ENABLED=false \
+  --log-driver json-file \
+  --log-opt max-size=10m \
+  -p 9091:9091 \
+  haugene/transmission-openvpn
 
 
 
