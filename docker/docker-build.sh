a7e58dc95ce403a4e95c9187384b1ec5d29d7e57
sstassin <sstassin@gmail.com>
2019-01-11 21:59:45 +0100

    Update docker-build.sh

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -32,3 +32,24 @@ docker run --name=transmission-ws --cap-add=NET_ADMIN --device=/dev/net/tun \
   -p 9091:9091 \
   sstassin/docker-transmission-openvpn
 
+#With port 443,80,53,1194,54783 and custom DNS Windscribe 10.255.255.1
+docker container stop transmission && docker container rm transmission
+  docker run --name=transmission --cap-add=NET_ADMIN --device=/dev/net/tun \
+  --dns 208.67.222.222 --dns 208.67.222.220 --restart='always' -d \
+  -v /media/HDD1000G/:/data \
+  -v /etc/localtime:/etc/localtime:ro \
+  -e OPENVPN_PROVIDER=WINDSCRIBE \
+  -e OPENVPN_CONFIG=Norway-udp \
+  -e OPENVPN_USERNAME=$WSVPNUSERNAME \
+  -e OPENVPN_PASSWORD=$WSVPNPWD \
+  -e LOCAL_NETWORK=192.168.0.0/24 \
+  -e ENABLE_UFW=false \
+  -e OPENVPN_OPTS="--inactive 3601 --ping 10 --ping-exit 60" \
+  -e DROP_DEFAULT_ROUTE=true \
+  -e TRANSMISSION_DOWNLOAD_DIR="/data/Torrents-Downloads" \
+  -e TRANSMISSION_INCOMPLETE_DIR_ENABLED=false \
+  -e MONITORDNS_ENABLED=true \
+  --log-driver json-file \
+  --log-opt max-size=10m \
+  -p 9091:9091 \
+  sstassin/docker-transmission-openvpn:armhf
